172dce32183e53998fb7e2d22078406417f67920
JakeCormier <jake.r.cormier@gmail.com>
2017-09-29 13:16:13 -0600

    first push

---
+++ classes/inventory.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name, typeof, description, prop):
+        self.name = name
+        self.typeof = typeof
+        self.description = description
+        self.prop = prop
